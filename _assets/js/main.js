ff9555711bcbad5cc6cac45e0378e92754818a1e
hamishgray <hamish.gray@secretescapes.com>
2019-08-02 16:07:59 +0100

    dev on layout and adding copy

--- _assets/js/main.js
+++ _assets/js/main.js
@@ -1,1 +1,19 @@
 // general js for the project that wouldn't be a reuseable component
+
+var lastScrollTop = 0;
+$(window).scroll(function(event){
+   var st = $(this).scrollTop();
+   if (st > 50){
+     $('.site-header').addClass('scrolled');
+   }else{
+     $('.site-header').removeClass('scrolled');
+   }
+   if (st > lastScrollTop){
+     $('.site-header').addClass('hide');
+   } else {
+     $('.site-header').removeClass('hide');
+   }
+   lastScrollTop = st;
+});
+
+
